59f796800940a5398a66ae5c5526aeb29a50eb47
Steven Wyks <38201284+slw515@users.noreply.github.com>
2019-02-13 12:16:22 +0400

    Add files via upload

---
+++ 30MFF Website/register.js
@@ -0,0 +1,15 @@
+//var inp = document.getElementbyId("buttons")
+//
+function clicked() {
+  alert('thanks for signing up!');
+};
+//
+// document.getElementById("button").onclick = function () {
+//         location.href = "https://www.youtube.com";
+//     };
+// function redirect() {
+//   var url = "about.html";
+// }
+function basket() {
+  alert('Your Purchase is Complete!');
+};
